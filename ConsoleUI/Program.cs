b0fde534bc40f642809ff5a1f3dd1916516383b0
Maryia Serbenko <serbenko.m@gmail.com>
2015-12-05 16:50:46 +0300

    solution for task1 ex.1

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NewtonMethod;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number, power;
+            do
+            {
+                Console.WriteLine("Enter a number:");
+            } while(!Double.TryParse(Console.ReadLine(), out number));
+
+            do
+            {
+                Console.WriteLine("Enter a power:");
+            } while (!Double.TryParse(Console.ReadLine(), out power));
+            Console.WriteLine("Newton Method: " + NewtonMethodClass.GetRootByNewtonMethod(power, number));
+            if (power != 0)
+                power = 1 / power;
+            Console.WriteLine("Math.Pow: " + (double) Math.Pow(number,power));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NewtonMethod/NewtonMethodClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonMethod
+{
+    public class NewtonMethodClass
+    {
+        public static double GetRootByNewtonMethod(double power, double number)
+        {
+            if (power == 0)
+                return 1;
+            if (number == 0 && power > 0)
+                return 0;
+            if (number == 0 && power < 0)
+                return Double.PositiveInfinity;
+            if (number < 0 && (power % 2) == 0)
+                return Double.NaN;
+
+            if (power < 0)
+            {
+                power *= -1;
+                number = 1 / number;
+            }
+            double accuracy = 1e-10;
+            double x = 1;
+            for (;;)
+            {
+                double nx = (((power - 1) * x) + (number / Math.Pow(x, (power - 1))))/power;
+                if (Math.Abs(x - nx) < accuracy) break;
+                x = nx;
+            }
+            return x;
+        }
+    }
+}
